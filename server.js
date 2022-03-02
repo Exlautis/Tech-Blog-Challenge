a83c367d1f10c3b7e0a413cac1010f5df5a6e244
Xavyr Rodriguez <xavyrrodriguez@Xavyrs-Air.attlocal.net>
2022-03-01 20:36:08 -0600

    set up connection and added user model

---
+++ server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+const express = require('express');
+const session = require('express-session');
+const sequelize = require('./config/connection');
+
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now Listening :p'))
+});
