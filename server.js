d286675dce7426115754a3548c0ead01d01ed04d
Xavyr Rodriguez <xavyrrodriguez@Xavyrs-MacBook-Air.local>
2022-03-08 01:07:10 -0600

    updates to try to fix server connection

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sess = {
-    secret: 'Super secret secret',
+    secret: 'Super secret',
     cookie: {},
     resave: false,
     saveUninitialized: true,
